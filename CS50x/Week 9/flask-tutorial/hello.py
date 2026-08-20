cb51a525b1b10a17ea353b7d4dd662430d3a280a
Pawel <pawelbabiak@gmail.com>
2026-08-20 22:49:05 +0100

    Implement dynamic greeting in Flask app and add index.html template

--- CS50x/Week 9/flask-tutorial/hello.py
+++ CS50x/Week 9/flask-tutorial/hello.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return '<strong>Hello, World!!!</strong>'
+def index():
+    if 'name' in request.args:
+        name = request.args['name']
+    else:
+        name = 'world!!!'
+    return render_template('index.html', placeholder=name)
 
 app.run(debug=True)
